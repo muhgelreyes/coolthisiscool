93bc75148e5348c1742e138cca15e09eec9921f4
Miguel <miguelreyes@ufl.edu>
2018-10-16 09:58:42 -0400

    First Commit

---
+++ IdeaProjects/Gitlab/src/Gitlab.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Gitlab
+{
+    public static void main(String[] args)
+    {
+            Scanner scanner = new Scanner(System.in);
+            String name;
+
+            System.out.print("Hello. What is your name? ");
+            name = scanner.next();
+    }
+}
